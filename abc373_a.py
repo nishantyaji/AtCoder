83b186f5bb7b01b4f2478b40fa9c164f24d11255
nishantyaji <nishant.yaji@gmail.com>
2024-09-29 23:48:45 +0530

    Solution to the AtCoder Beginner Contest 373 Problem A

--- abc373_a.py
+++ abc373_a.py
@@ -1,24 +1,5 @@
-import platform
-
 in_fn = input
 op_fn = print
-
-input_file = platform.node() and platform.node().startswith("LAPTOP-")
-if input_file:
-    filename = "abc373_a.txt"
-    f = open(filename, "r")
-    in_fn = f.readline
-
-
-    def check_file(args):
-        exp = f.readline().strip()
-        if str(args) == exp:
-            print("Pass")
-        else:
-            print("Fail. Expected:", exp, "Got", args)
-
-
-    op_fn = check_file
 
 
 def read_str() -> str:
