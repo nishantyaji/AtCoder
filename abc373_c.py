25515383945481bdab7e719a9b6840c13325d2e8
nishantyaji <nishant.yaji@gmail.com>
2024-09-29 23:50:03 +0530

    Solution to the AtCoder Beginner Contest 373 Problem C

---
+++ abc373_c.py
@@ -0,0 +1,14 @@
+in_fn = input
+op_fn = print
+
+
+def read_int() -> int:
+    return int(in_fn().strip())
+
+
+def read_int_arr(sep=" ") -> list[int]:
+    return list(map(int, in_fn().strip().split(sep)))
+
+
+num = read_int()
+op_fn(max(read_int_arr()) + max(read_int_arr()))
